65d193fe5d06c38df2ba5ae6b9536f81a1345179
luizcarloskazuyukifukaya <luizcarloskazuyukifukaya@gmail.com>
2023-04-14 06:03:45 +0900

    update helm install workflow

--- seed-data/generate-votes.sh
+++ seed-data/generate-votes.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-APP_NAME="woven-challenge"
 LB_PORT=80
 VOTE_IP=$(kubectl get svc vote-extlb --no-headers | awk '{print $4}')
 RESULT_IP=$(kubectl get svc result-extlb --no-headers | awk '{print $4}')
@@ -12,13 +11,7 @@ RESULT_SERVER="$RESULT_IP:$LB_PORT"
 echo "Vote URL is http://$RESULT_IP:$LB_PORT"
 echo "Vote Server is $RESULT_SERVER"
 
-# define STATIC VOTE_SERVER
-#export VOTE_SERVER="34.31.121.84:80"
-
-# create 3000 votes (2000 for option a, 1000 for option b)
-#ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote/
-#ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://vote/
-#ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote/
+# create 300 votes (200 for option a, 100 for option b)
 ab -n 100 -c 50 -p posta -T "application/x-www-form-urlencoded" http://$VOTE_SERVER/
 ab -n 100 -c 50 -p postb -T "application/x-www-form-urlencoded" http://$VOTE_SERVER/
 ab -n 100 -c 50 -p posta -T "application/x-www-form-urlencoded" http://$VOTE_SERVER/
