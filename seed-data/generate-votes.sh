e8cd81e96d65ee8163101fafd3fa0dea7f8a2cb4
luizcarloskazuyukifukaya <luizcarloskazuyukifukaya@gmail.com>
2023-04-15 08:36:19 +0900

    Updated seed data script

--- seed-data/generate-votes.sh
+++ seed-data/generate-votes.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 LB_PORT=80
-VOTE_IP=$(kubectl get svc vote-extlb --no-headers | awk '{print $4}')
-RESULT_IP=$(kubectl get svc result-extlb --no-headers | awk '{print $4}')
+NS="default"
+VOTE_IP=$(kubectl get svc vote-extlb --no-headers -n $NS | awk '{print $4}')
+RESULT_IP=$(kubectl get svc result-extlb --no-headers -n $NS | awk '{print $4}')
 
 VOTE_SERVER="$VOTE_IP:$LB_PORT"
 echo "Vote URL is http://$VOTE_IP:$LB_PORT"
 echo "Vote Server is $VOTE_SERVER"
 
 RESULT_SERVER="$RESULT_IP:$LB_PORT"
-echo "Vote URL is http://$RESULT_IP:$LB_PORT"
-echo "Vote Server is $RESULT_SERVER"
+echo "Result URL is http://$RESULT_IP:$LB_PORT"
+echo "Result Server is $RESULT_SERVER"
+
+# Create Data
+python3 make-data.py
 
 # create 300 votes (200 for option a, 100 for option b)
 ab -n 100 -c 50 -p posta -T "application/x-www-form-urlencoded" http://$VOTE_SERVER/
 ab -n 100 -c 50 -p postb -T "application/x-www-form-urlencoded" http://$VOTE_SERVER/
 ab -n 100 -c 50 -p posta -T "application/x-www-form-urlencoded" http://$VOTE_SERVER/
+
+# Clean Data
+rm posta
+rm postb
