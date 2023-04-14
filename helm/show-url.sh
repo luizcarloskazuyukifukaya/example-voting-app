5635a1d01b8a1283be532621a4a64b542e8648de
luizcarloskazuyukifukaya <luizcarloskazuyukifukaya@gmail.com>
2023-04-15 08:11:41 +0900

    Updated Show URL script

--- helm/show-url.sh
+++ helm/show-url.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+gcloud container clusters get-credentials luiz-devops-cicd-k8s --region us-central1 --project luiz-devops-cicd
+
 LB_PORT=80
-VOTE_IP=$(kubectl get svc vote-extlb --no-headers | awk '{print $4}')
-RESULT_IP=$(kubectl get svc result-extlb --no-headers | awk '{print $4}')
+NS="default"
+VOTE_IP=$(kubectl get svc vote-extlb --no-headers -n $NS | awk '{print $4}')
+RESULT_IP=$(kubectl get svc result-extlb --no-headers -n $NS | awk '{print $4}')
 
 VOTE_SERVER="$VOTE_IP:$LB_PORT"
 echo "Vote URL is http://$VOTE_IP:$LB_PORT"
