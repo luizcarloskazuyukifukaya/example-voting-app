45cb54b4d1d2c3a8ce645e4f9812fa4f3382cc53
luizcarloskazuyukifukaya <luizcarloskazuyukifukaya@gmail.com>
2023-04-15 08:49:43 +0900

    Revert "updated seed data script - All YAML exist"
    This reverts commit 9cdfaa5c3779c84d8e7591abf40c1b63c4ec3f38.

--- seed-data/generate-votes.sh
+++ seed-data/generate-votes.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 LB_PORT=80
-NS="examples" # argocd version is not on default
-VOTE_IP=$(kubectl get svc vote-extlb --no-headers -n $NS| awk '{print $4}')
-RESULT_IP=$(kubectl get svc result-extlb --no-headers -n $NS| awk '{print $4}')
+VOTE_IP=$(kubectl get svc vote-extlb --no-headers | awk '{print $4}')
+RESULT_IP=$(kubectl get svc result-extlb --no-headers | awk '{print $4}')
 
 VOTE_SERVER="$VOTE_IP:$LB_PORT"
 echo "Vote URL is http://$VOTE_IP:$LB_PORT"
@@ -12,14 +11,7 @@ RESULT_SERVER="$RESULT_IP:$LB_PORT"
 echo "Vote URL is http://$RESULT_IP:$LB_PORT"
 echo "Vote Server is $RESULT_SERVER"
 
-# generate data
-python3 make-data.py
-
 # create 300 votes (200 for option a, 100 for option b)
 ab -n 100 -c 50 -p posta -T "application/x-www-form-urlencoded" http://$VOTE_SERVER/
 ab -n 100 -c 50 -p postb -T "application/x-www-form-urlencoded" http://$VOTE_SERVER/
 ab -n 100 -c 50 -p posta -T "application/x-www-form-urlencoded" http://$VOTE_SERVER/
-
-# Clean data
-rm posta
-rm postb
